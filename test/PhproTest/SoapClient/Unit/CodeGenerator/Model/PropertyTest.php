980c3221ccd523303be709d72a65771abf0fb872
Toon Verwerft <toonverwerft@gmail.com>
2025-10-23 08:31:52 +0200

    Fix xsd:language-like simple type generations.

--- test/PhproTest/SoapClient/Unit/CodeGenerator/Model/PropertyTest.php
+++ test/PhproTest/SoapClient/Unit/CodeGenerator/Model/PropertyTest.php
@@ -3,10 +3,12 @@
 namespace PhproTest\SoapClient\Unit\CodeGenerator\Model;
 
 use Phpro\SoapClient\CodeGenerator\Model\Property;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 use Psl\Option\Option;
 use Soap\Engine\Metadata\Model\Property as EngineProperty;
+use Soap\Engine\Metadata\Model\TypeMeta;
 use Soap\Engine\Metadata\Model\XsdType;
 
 /**
@@ -56,5 +58,63 @@ class PropertyTest extends TestCase
         self::assertNotSame($xsdType, $property->getXsdType());
         self::assertSame('DateInterval', $property->getXsdType()->getName());
         self::assertSame('DateInterval', $property->getXsdType()->getBaseType());
+    }
+
+    #[Test]
+    #[DataProvider('provideTypes')]
+    public function it_can_figure_out_the_type(Property $property, string $expectedType): void
+    {
+        self::assertSame($expectedType, $property->getType());
+    }
+
+    public static function provideTypes()
+    {
+        yield 'known_simple_type' => [
+            Property::fromMetaData(
+                'MyApp',
+                new EngineProperty(
+                    'property',
+                    XsdType::create('string')
+                )
+            ),
+            'string'
+        ];
+        yield 'known_simple_base_type' => [
+            Property::fromMetaData(
+                'MyApp',
+                new EngineProperty(
+                    'property',
+                    XsdType::create('language')
+                        ->withXmlNamespace('http://www.w3.org/2001/XMLSchema')
+                        ->withXmlNamespaceName('xsd')
+                        ->withBaseType('string')
+                        ->withMemberTypes(['token'])
+                        ->withMeta(static fn (TypeMeta $meta) => $meta->withIsSimple(true))
+                )
+            ),
+            'string'
+        ];
+
+        yield 'root namespace' => [
+            Property::fromMetaData(
+                '',
+                new EngineProperty(
+                    'property',
+                    XsdType::create('MyType')
+                )
+            ),
+            '\\MyType'
+        ];
+
+        yield 'namespaced' => [
+            Property::fromMetaData(
+                'MyApp',
+                new EngineProperty(
+                    'property',
+                    XsdType::create('MyType')
+                )
+            ),
+            '\\MyApp\\MyType'
+        ];
     }
 }
